e9b72b2eb7adac633998dd4a674dcc7961d62bd6
Alexey Busarev <busarevai03@gmail.com>
2025-01-17 15:04:27 +0400

    stable

--- pdf_to_docx.py
+++ pdf_to_docx.py
@@ -1,33 +1,27 @@
-# скрипт конвертации (до 14 строки)
 from pdf2docx import Converter
-
-
-def convert_pdf_to_docx(pdf_file, docx_file):
-
-    # Создание объекта Converter
-    cv = Converter(pdf_file)
-
-    # Конвертация указанной страницы PDF в docx 
-    cv.convert(docx_file, start=0, end=None)
-    cv.close()
-
-# Конвертация PDF в файл Docx
-convert_pdf_to_docx("", "")
-
-# скрипт UI
 import tkinter as tk
 from tkinter import filedialog as fd 
 
 
 def callback():
-    name= fd.askopenfilename(filetypes=[("Файлы .pdf", "*.pdf")]) 
-# создаем ворд док с названием пдф файла, в который мы будем всю инфу пихать
-#    file = open(f'{name[0:-4]}.docx', 'w')
-# закрываем файл и он сохраняется
-#     file.close()
-# строка, чтобы понять, как будет называться файл
-    print(f'{name[0:-4]}.docx')
+    # "Открываем" .pdf файл
+    pdf_name= fd.askopenfilename(filetypes=[("Файлы .pdf", "*.pdf")]) 
+    
+    # Создаем название .docx файлу на основе .pdf
+    docx_name = f'{pdf_name[0:-4]}.docx'
 
+    # Создаем .docx файл и сохраняем его для дальнейших манипуляций
+    file = open(docx_name, 'w')
+
+    # Закрываем созданный файл для сохранения
+    file.close()
+
+    # Создание объекта Converter
+    cv = Converter(pdf_name)
+
+    # Конвертация указанной страницы PDF в docx
+    cv.convert(docx_name, start=0, end=None)
+    cv.close()
 
 root = tk.Tk()
 root.title(".pdf в .docx")
